2f8b0d74f8f9becf2f4a15145a5a429764cec572
Andrew Zelenets <andrew.zelenets@gmail.com>
2015-09-07 14:07:56 +0300

    Add Country model

---
+++ app/models/country.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: countries
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  code       :string
+#  visited    :boolean          default(FALSE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Country < ActiveRecord::Base
+  # Scopes
+  scope :not_visited, -> { where(visited: false) }
+  scope :visited, -> { where(visited: true) }
+
+  # Associations
+  has_and_belongs_to_many :currencies
+end
