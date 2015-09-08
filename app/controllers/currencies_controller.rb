dca0da9ca64851a26db7ac2f4da99262dd6a0c89
Andrew Zelenets <andrew.zelenets@gmail.com>
2015-09-08 16:02:18 +0300

    Filtering currencies from the list - currencies to those that still have countries to visit.

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -4,6 +4,8 @@ class CurrenciesController < ApplicationController
       @currencies = Currency.includes(:countries).collected
     elsif params[:not_collected]
       @currencies = Currency.includes(:countries).not_collected
+    elsif params[:country_not_visited]
+      @currencies = Currency.includes(:countries).where(countries: {visited: false}).all
     else
       @currencies = Currency.includes(:countries).all
     end
